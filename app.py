ecaff3a8b897915855d516ffdb99dc7102173b09
Nattakonjpg <108257430+Nattakonjpg@users.noreply.github.com>
2024-03-01 17:18:09 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+    
+UPLOAD_FOLDER = 'uploads'  # 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/home_web')
 def home():    
@@ -11,8 +14,20 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/webapp') 
+@app.route('/webapp', methods=['GET', 'POST']) 
 def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload'))
     return render_template('webapp.html')
 
 @app.route('/creator')
