a0f93ca5270b089323dd1a0b1d4a6a49d7cf60e3
TungSoPon <108257588+TungSoPon@users.noreply.github.com>
2024-03-04 11:02:50 +0700

    Delete static/js/Video.java

--- static/js/Video.java
+++
@@ -1,4 +0,0 @@
-document.getElementById('upload-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // ป้องกันการส่งคำขอฟอร์มโดยปกติ
-    uploadAndDisplayVideo(event);
-});
