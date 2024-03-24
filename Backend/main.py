ccb7fb72b6a07494c73a71d619fd6e0d0621cb78
Nattakonjpg <108257430+Nattakonjpg@users.noreply.github.com>
2024-03-25 05:18:06 +0700

    Add files via upload

--- Backend/main.py
+++ Backend/main.py
@@ -45,42 +45,44 @@ def load_selected_model():
     selected_model_path = request.json['selectedModelPath']
     try:
         loaded_model = load_model(selected_model_path)
-        return 'Model loaded successfully!', 200
+        # ระบุ path ของวิดีโอที่ต้องการตัด
+        inputVideo = r"/home/nattakonpu/codes/Valolyze/Backend/video_data/test_videofull/agent/round/รอบ 1.avi"
+        # ระบุ path ที่ต้องการบันทึกไฟล์ผลลัพธ์
+        outputFolderVideo = r"/home/nattakonpu/codes/Valolyze/Backend/video_data/test_cutvideo/agent/round/test/"
+        # ระบุความยาวของคลิปวิดีโอที่ต้องการตัด (วินาที)
+        clipDuration = 2
+        # ระบุ path ที่ต้องการบันทึกไฟล์ CSV
+        output_csv = r"/home/nattakonpu/codes/Valolyze/Backend/Output/Time/agent/round1.csv"
+
+        cut_video(inputVideo, outputFolderVideo, clipDuration, output_csv)
+        df = pd.read_csv(output_csv)
+        print(df.tail())
+
+        # เรียกใช้งาน function
+        # outputFolderVideo = มาจาก video ตัดซอย 2 วิแล้วใช้ folder เดียวกัน
+        output_file_text = r"/home/nattakonpu/codes/Valolyze/Backend/test_train_splits/agent/round/test_test_split1.txt"
+
+        create_test_split(outputFolderVideo, output_file_text)
+
+        data_dir = r"/home/nattakonpu/codes/Valolyze/Backend/video_data/test_cutvideo/agent/round/"
+        split_dir = r"/home/nattakonpu/codes/Valolyze/Backend/test_train_splits/agent/round/"
+        test_loader_A5 = prepare_hmdb51_test_loader(data_dir, split_dir)
+        print(test_loader_A5.dataset)
+
+        # ระบุเส้นทางที่จะบันทึกผลลัพธ์
+        output_path = r"/home/nattakonpu/codes/Valolyze/Backend/Output/Predict/agent/Round1.csv"
+        
+        # เรียกใช้ฟังก์ชัน evaluate_model
+        evaluate_model(loaded_model, test_loader_A5, output_path)
+        
+        path1 = output_path
+        path2 = output_csv
+        path3 = r"/home/nattakonpu/codes/Valolyze/Backend/Output/Final/FinalPredict+time_Round_1.csv"
+        combine_and_transform_csv(path1, path2, path3)
+        
+        return 'Model loaded and evaluated successfully!', 200
     except Exception as e:
-        return f'Failed to load model. Error: {str(e)}', 500
+        return f'Failed to load or evaluate model. Error: {str(e)}', 500
 
 if __name__ == '__main__':
-    # ระบุ path ของวิดีโอที่ต้องการตัด
-    inputVideo = r"/home/nattakonpu/codes/Valolyze/Backend/video_data/test_videofull/agent/round/รอบ 1.avi"
-    # ระบุ path ที่ต้องการบันทึกไฟล์ผลลัพธ์
-    outputFolderVideo = r"/home/nattakonpu/codes/Valolyze/Backend/video_data/test_cutvideo/agent/round/test/"
-    # ระบุความยาวของคลิปวิดีโอที่ต้องการตัด (วินาที)
-    clipDuration = 2
-    # ระบุ path ที่ต้องการบันทึกไฟล์ CSV
-    output_csv = r"/home/nattakonpu/codes/Valolyze/Backend/Output/Time/agent/round1.csv"
-
-    cut_video(inputVideo, outputFolderVideo, clipDuration, output_csv)
-    df = pd.read_csv(output_csv)
-    print(df.tail())
-
-    # เรียกใช้งาน function
-    # outputFolderVideo = มาจาก video ตัดซอย 2 วิแล้วใช้ folder เดียวกัน
-    output_file_text = r"/home/nattakonpu/codes/Valolyze/Backend/test_train_splits/agent/round/test_test_split1.txt"
-
-    create_test_split(outputFolderVideo, output_file_text)
-
-    data_dir = r"/home/nattakonpu/codes/Valolyze/Backend/video_data/test_cutvideo/agent/round/"
-    split_dir = r"/home/nattakonpu/codes/Valolyze/Backend/test_train_splits/agent/round/"
-    test_loader_A5 = prepare_hmdb51_test_loader(data_dir, split_dir)
-    print(test_loader_A5.dataset)
-
-    # ระบุเส้นทางที่จะบันทึกผลลัพธ์
-    output_path = r"/home/nattakonpu/codes/Valolyze/Backend/Output/Predict/agent/Round1.csv"
-    # เรียกใช้ฟังก์ชัน
-    evaluate_model(loaded_model, test_loader_A5, output_path)
-
-    path1 = output_path
-    path2 = output_csv
-    path3 = r"/home/nattakonpu/codes/Valolyze/Backend/Output/Final/FinalPredict+time_Round_1.csv"
-    combine_and_transform_csv(path1, path2, path3)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
