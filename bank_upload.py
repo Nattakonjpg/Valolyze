b0d7aa98b770edf97c05ad5db094e93287716a1d
Nattakonjpg <108257430+Nattakonjpg@users.noreply.github.com>
2024-03-30 22:56:45 +0700

    Add files via upload

--- bank_upload.py
+++ bank_upload.py
@@ -5,12 +5,9 @@ import subprocess
 
 app = Flask(__name__)
 
-# ระบุโฟลเดอร์ที่จะเก็บวิดีโอ
-UPLOAD_FOLDER = '/home/nattakonpu/codes/Valolyze/Backend/video_data/test_videofull/agent/round'
-# ระบุโฟลเดอร์ที่มีไฟล์ที่ต้องการส่งออก
-OUTPUT_FOLDER = '/home/nattakonpu/codes/Valolyze/Backend/Output/Final/'
-# โฟลเดอร์ที่ 2 เพื่อเก็บวิดีโอที่จะใช้แสดงในเว็บ
-STATIC_IMG_FOLDER = '/home/nattakonpu/codes/Valolyze/static/img/'
+UPLOAD_FOLDER = '/home/nattakonpu/codes/Valolyze/Backend/video_data/test_videofull/agent/round'  # ระบุโฟลเดอร์ที่จะเก็บวิดีโอ
+OUTPUT_FOLDER = '/home/nattakonpu/codes/Valolyze/Backend/Output/Final/'  # ระบุโฟลเดอร์ที่มีไฟล์ที่ต้องการส่งออก
+STATIC_IMG_FOLDER = '/home/nattakonpu/codes/Valolyze/static/img/'  # โฟลเดอร์ที่ 2 เพื่อเก็บวิดีโอที่จะใช้แสดงในเว็บ
 
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -22,46 +19,21 @@ app.config['STATIC_IMG_FOLDER'] = STATIC_IMG_FOLDER
 def check_model_file_existence(model_file_path):
     return os.path.exists(model_file_path)
 
-
 @app.route('/home_web')
-def home():
+def home():    
     return render_template("New_index.html")
-
 
 @app.route('/about')
 def about():
     return render_template('New_about.html')
 
-
-@app.route('/webapp')
+@app.route('/webapp') 
 def upload():
     return render_template('bank.html')
-
-
-@app.route('/Sage.html')
-def upload2():
-    return render_template('Sage.html')
-
-
-@app.route('/Sova.html')
-def upload3():
-    return render_template('Sova.html')
-
-
-@app.route('/Jett.html')
-def upload4():
-    return render_template('Jett.html')
-
-
-@app.route('/Habor.html')
-def upload5():
-    return render_template('Habor.html')
-
 
 @app.route('/creator')
 def creator():
     return render_template('Creator.html')
-
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -70,104 +42,25 @@ def upload_file():
         if file:
             filename = file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # บันทึกไฟล์วิดีโอไว้ในโฟลเดอร์สำหรับการแสดงในเว็บด้วย
-            file.save(os.path.join(app.config['STATIC_IMG_FOLDER'], filename))
+            file.save(os.path.join(app.config['STATIC_IMG_FOLDER'], filename))  # บันทึกไฟล์วิดีโอไว้ในโฟลเดอร์สำหรับการแสดงในเว็บด้วย
 
-            # เรียกใช้หลัง main.py หลังอัพโหลดเสร็จ
-            subprocess.Popen(
-                ["python", "/home/nattakonpu/codes/Valolyze/Backend/main.py"])
+            #เรียกใช้หลัง main.py หลังอัพโหลดเสร็จ
+            subprocess.Popen(["python", "/home/nattakonpu/codes/Valolyze/Backend/main.py"])
 
             subprocess.Popen(["curl", "http://localhost:5001/fetch_data"])
             return redirect(url_for('home'))
-
-
-@app.route('/upload2', methods=['POST'])
-def upload_file2():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # บันทึกไฟล์วิดีโอไว้ในโฟลเดอร์สำหรับการแสดงในเว็บด้วย
-            file.save(os.path.join(app.config['STATIC_IMG_FOLDER'], filename))
-
-            # เรียกใช้หลัง main.py หลังอัพโหลดเสร็จ
-            subprocess.Popen(
-                ["python", "/home/nattakonpu/codes/Valolyze/Backend/main2.py"])
-
-            subprocess.Popen(["curl", "http://localhost:5001/fetch_data"])
-            return redirect(url_for('home'))
-
-
-@app.route('/upload3', methods=['POST'])
-def upload_file3():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # บันทึกไฟล์วิดีโอไว้ในโฟลเดอร์สำหรับการแสดงในเว็บด้วย
-            file.save(os.path.join(app.config['STATIC_IMG_FOLDER'], filename))
-
-            # เรียกใช้หลัง main.py หลังอัพโหลดเสร็จ
-            subprocess.Popen(
-                ["python", "/home/nattakonpu/codes/Valolyze/Backend/main3.py"])
-
-            subprocess.Popen(["curl", "http://localhost:5001/fetch_data"])
-            return redirect(url_for('home'))
-
-
-@app.route('/upload4', methods=['POST'])
-def upload_file4():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # บันทึกไฟล์วิดีโอไว้ในโฟลเดอร์สำหรับการแสดงในเว็บด้วย
-            file.save(os.path.join(app.config['STATIC_IMG_FOLDER'], filename))
-
-            # เรียกใช้หลัง main.py หลังอัพโหลดเสร็จ
-            subprocess.Popen(
-                ["python", "/home/nattakonpu/codes/Valolyze/Backend/main4.py"])
-
-            subprocess.Popen(["curl", "http://localhost:5001/fetch_data"])
-            return redirect(url_for('home'))
-
-
-@app.route('/upload5', methods=['POST'])
-def upload_file5():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # บันทึกไฟล์วิดีโอไว้ในโฟลเดอร์สำหรับการแสดงในเว็บด้วย
-            file.save(os.path.join(app.config['STATIC_IMG_FOLDER'], filename))
-
-            # เรียกใช้หลัง main.py หลังอัพโหลดเสร็จ
-            subprocess.Popen(
-                ["python", "/home/nattakonpu/codes/Valolyze/Backend/main5.py"])
-
-            subprocess.Popen(["curl", "http://localhost:5001/fetch_data"])
-            return redirect(url_for('home'))
-
 
 @app.route('/download')
 def download_final_predict():
-    file_path = os.path.join(
-        app.config['OUTPUT_FOLDER'], 'FinalPredict+time_Round_1.csv')
+    file_path = os.path.join(app.config['OUTPUT_FOLDER'], 'FinalPredict+time_Round_1.csv')
     return send_file(file_path, as_attachment=True)
-
 
 @app.route('/fetch_data')
 def fetch_data():
-    file_path = os.path.join(
-        app.config['OUTPUT_FOLDER'], 'FinalPredict+time_Round_1.csv')
+    file_path = os.path.join(app.config['OUTPUT_FOLDER'], 'FinalPredict+time_Round_1.csv')
     with open(file_path, 'r') as f:
         data = f.read()
     return jsonify(data=data)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5001)
